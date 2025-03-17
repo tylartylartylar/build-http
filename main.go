eb0de9dda8068f7ebd4c755e5568fb14d04fe0dd
tylartylartylar <tylar@tylar.today>
2025-03-17 15:48:13 -0500

    Refactor to make reusable function from a tcp connection

--- main.go
+++ main.go
@@ -3,43 +3,56 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+
+		data := make([]byte, 8)
+		currentLine := ""
+		reader := bufio.NewReader(f)
+
+		for {
+			n, err := reader.Read(data)
+			if err != nil {
+				if err.Error() != "EOF" {
+					fmt.Println("Error reading file:", err)
+				}
+				break
+			}
+
+			parts := string(data[:n])
+			for _, part := range parts {
+				if part == '\n' {
+					lines <- currentLine
+					currentLine = ""
+				} else {
+					currentLine += string(part)
+				}
+			}
+		}
+
+		if currentLine != "" {
+			lines <- currentLine
+		}
+		close(lines)
+	}()
+	return lines
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer file.Close()
-
-	data := make([]byte, 8)
-	currentLine := ""
-	reader := bufio.NewReader(file)
-
-	for {
-		n, err := reader.Read(data)
-		if err != nil {
-			if err.Error() != "EOF" {
-				fmt.Println("Error reading file:", err)
-			}
-			break
-		}
-
-		parts := string(data[:n])
-		for _, part := range parts {
-			if part == '\n' {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			} else {
-				currentLine += string(part)
-			}
-		}
-	}
-
-	if currentLine != "" {
-		fmt.Printf("read: %s\n", currentLine)
+	linesChannel := getLinesChannel(file)
+	for line := range linesChannel {
+		fmt.Printf("read: %s\n", line)
 	}
 }
